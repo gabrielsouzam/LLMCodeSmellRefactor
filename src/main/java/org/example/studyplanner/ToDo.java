e1045a3aacee3ac06a80c9a1c931b5711906277e
gabrielmendes64 <gabriel10010100@gmail.com>
2025-01-09 20:45:18 -0300

    fix: refactor code smell data class using encapsulate behavior refactoring technique

--- src/main/java/org/example/studyplanner/ToDo.java
+++ src/main/java/org/example/studyplanner/ToDo.java
@@ -1,12 +1,15 @@
 package org.example.studyplanner;
 
 import java.text.MessageFormat;
+import java.time.LocalDate;
+import java.util.Objects;
 
 public class ToDo implements PlannerMaterial{
     private Integer id;
     private String title;
     private String description;
     private int priority;
+    private boolean completed;
 
     public ToDo(Integer id, String title, String description, int priority) {
         this.id = id;
@@ -50,5 +53,58 @@ public class ToDo implements PlannerMaterial{
 
     public void setPriority(int priority) {
         this.priority = priority;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    public boolean isActive() {
+        return !completed;
+    }
+
+    public String getImportance() {
+        if (priority == 1) {
+            return "Baixa";
+        } else if (priority == 2) {
+            return "Média";
+        } else {
+            return "Alta";
+        }
+    }
+
+    public int compareTo(ToDo other) {
+        return Integer.compare(this.priority, other.priority);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ToDo todo = (ToDo) o;
+        return id.equals(todo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int compareByTitle(ToDo other) {
+        return this.title.compareTo(other.title);
+    }
+
+    public boolean belongsToCategory(String category) {
+        // Assumindo que a descrição contenha informações sobre a categoria
+        return description.contains(category);
+    }
+
+    public String getSummary() {
+        return title + " (" + priority + ")";
+    }
+
+    // Assumindo que o título contenha o nome do projeto
+    public boolean isRelatedToProject(String projectName) {
+        return title.contains(projectName);
     }
 }
