a67386be2a1944bbb775cf2e6642653da8f14558
gabrielmendes64 <gabriel10010100@gmail.com>
2025-01-09 20:59:14 -0300

    fix: refactor code smell long method using extract method refactoring technique

--- src/main/java/org/example/studycards/Card.java
+++ src/main/java/org/example/studycards/Card.java
@@ -43,4 +43,11 @@ public class Card {
     public double calculateSuccessRate(int totalQuestions, int correctAnswers) {
         return (double) correctAnswers / totalQuestions * 100;
     }
+
+    public String buildCardResponse(Integer randomCard) {
+        String response = "[" + randomCard + "] ";
+        response += "The random question was: " + this.question + " | ";
+        response += "The answer is: " + this.answer;
+        return response;
+    }
 }
