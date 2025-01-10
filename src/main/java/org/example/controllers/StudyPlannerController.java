33a1ac5a1820049ca947839e94511249db0bbdf3
EmanuelALemos <araujoemanuel97@gmail.com>
2025-01-10 14:42:40 -0300

    fix: refactor code smell Long Parameter List using Introduce Parameter Objecte refactoring technique

--- src/main/java/org/example/controllers/StudyPlannerController.java
+++ src/main/java/org/example/controllers/StudyPlannerController.java
@@ -116,8 +116,10 @@ public class StudyPlannerController {
         String motivation = Objects.requireNonNull(this.getInput().trim());
         Integer dailyMinutesDedication = Integer.parseInt(Objects.requireNonNull(this.getInput().trim()));
         Integer dailyHoursDedication = Integer.parseInt(Objects.requireNonNull(this.getInput().trim()));
-        LocalDateTime start =  handleGetStartDate();
-        habitTracker.addHabit(name, motivation, dailyMinutesDedication, dailyHoursDedication, start.getYear(), start.getMonthValue(), start.getDayOfMonth(), start.getHour(), start.getMinute(), start.getSecond(), false);
+        LocalDateTime start = handleGetStartDate();
+
+        // Call addHabit with complete LocalDateTime object
+        habitTracker.addHabit(name, motivation, dailyMinutesDedication, dailyHoursDedication, start, false);
     }
 
     private String viewToDoHeader(){
