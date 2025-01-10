fdbd19573716698ba59a0bc8abb730df28cda9b7
gabrielmendes64 <gabriel10010100@gmail.com>
2025-01-09 23:20:53 -0300

    fix: refactor code smell long method using extract method refactoring technique

--- src/main/java/org/example/studyplanner/TodoTracker.java
+++ src/main/java/org/example/studyplanner/TodoTracker.java
@@ -29,28 +29,33 @@ public class TodoTracker {
     public String toString() {
         StringBuilder str = new StringBuilder();
         for (ToDo toDo : toDos) {
-            String todoInfo = toDo.toString();
-            str.append(todoInfo);
-            str.append("\n");
-            Integer id = toDo.getId();
-            List<LocalDateTime> todosDate = this.tracker.get(id);
-            if(todosDate == null){
-                str.append("No tracks found\n");
-            }else{
-                for (LocalDateTime ldt : todosDate) {
-                    String pattern = "yyyy-MM-dd HH:mm:ss";
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-                    String formattedDate = formatter.format(ldt);
-                    str.append(formattedDate);
-                    str.append("\n");
-                }
-            }
+            str.append(toDo.toString()).append("\n");
+            str.append(getTrackInfo(toDo.getId())).append("\n");
         }
         String response = str.toString();
         if(response.isEmpty()){
             return "No ToDos found";
         }
         return response;
+    }
+
+    private String getTrackInfo(Integer id) {
+        List<LocalDateTime> todosDate = this.tracker.get(id);
+        if(todosDate == null){
+            return "No tracks found\n";
+        }else{
+            StringBuilder trackInfo = new StringBuilder();
+            for (LocalDateTime ldt : todosDate) {
+                trackInfo.append(formatLocalDateTime(ldt)).append("\n");
+            }
+            return trackInfo.toString();
+        }
+    }
+
+    private String formatLocalDateTime(LocalDateTime ldt) {
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return formatter.format(ldt);
     }
 
     public void addToDoExecutionTime(Integer id){
